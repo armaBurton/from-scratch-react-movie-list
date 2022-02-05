91aadd3238c00e312a9b4b736c396a4a3c958d18
armaBurton <armanarma@armaburton.com>
2022-02-04 16:12:55 -0800

    changes for netlify

--- src/CurrentMovie.js
+++ src/CurrentMovie.js
@@ -1,5 +1,4 @@
-export default function CurrentMovie(props, deleteMovie){
-  // console.log(props, deleteMovie);
+export default function CurrentMovie(props){
   return <div className='current-movie-card' style={{ backgroundColor: props.movie.color }} onClick={() => props.deleteMovie(props.movie.id)}>
     <h2>{props.movie.title}</h2>
     {
--- src/RenderMovies.js
+++ src/RenderMovies.js
@@ -2,7 +2,6 @@ import CurrentMovie from './CurrentMovie';
 
 export default function RenderMovies(props){
   const deleteMovie = props.deleteMovie;
-  console.log(props);
   return <>
     {
       props.movies.map((movie, i) => <CurrentMovie key={movie + i} movie={movie} deleteMovie={deleteMovie}/>)
